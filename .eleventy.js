b52223b7d113b66c6ffc79f0ce0ab787e7da1f2d
inkyvoxel <hello@inkyvoxel.com>
2024-01-19 10:32:23 +0000

    Add robots.txt & move everything to src folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,20 @@
 const { DateTime } = require("luxon");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
 	eleventyConfig.addFilter("readableDate", dateObj => {
 		return DateTime.fromJSDate(dateObj).toFormat("dd LLLL, yyyy");
 	});
 
-    eleventyConfig.addFilter("isoDate", dateObj => {
+	eleventyConfig.addFilter("isoDate", dateObj => {
 		return DateTime.fromJSDate(dateObj).toISO();
 	});
+
+	eleventyConfig.addPassthroughCopy({ './src/robots.txt': '/robots.txt' });
+
+	return {
+		dir: {
+		  input: "src",
+		},
+		passthroughFileCopy: true,
+	};
 }
